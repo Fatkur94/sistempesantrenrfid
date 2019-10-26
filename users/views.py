ef16a31b365bfd3cde2d905c378cdb8c47e82f3f
fatkur94 <zack@codemy.net>
2019-10-27 02:33:08 +0700

    fix profile

--- users/views.py
+++ users/views.py
@@ -73,7 +73,7 @@ def profile(request):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            username = form.cleaned_data.get('username')
+            username = u_form.cleaned_data.get('username')
             messages.success(request, f' Akun anda sudah diupdate!')
             return redirect('profile')
 
